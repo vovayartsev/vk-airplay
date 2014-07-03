16189181dcc05b871e91529b04685c46ea9c9be3
Vladimir Yartsev <vovayartsev@gmail.com>
2014-07-03 23:43:53 +0400

    Refactoring

--- vk-airplay-chrome/js/content.js
+++ vk-airplay-chrome/js/content.js
@@ -1,37 +1,37 @@
 var $audios_list = null;
 
+// HELPER FUNCTIONS
+
 function normalizeUrl(url) {
     return /(^.*)\?.*$/.exec(url)[1];
 }
+
+
+// DOM OPERATIONS
 
 // item to hook is passed as "this", b/o jQuery's 'each' notation
 function hookItem() {
     var $audio = $(this);
     $audio.addClass("airplay-hooked");
-    $audio.find(".area").prepend("<span class='airplay-button triangle'/>");
+    $audio.find(".area").prepend("<span class='airplay-button not-being-played'/>");
     setHandlers($audio);
 }
 
+// called periodically to find new (yet unhooked) audios in the DOM
 function rescanAndHookDom() {
     $audios_list.find(".audio:not(.airplay-hooked)").each(hookItem);
 }
 
+// extracts song title and URL from the DOM
 function scrap($audio) {
     var url = normalizeUrl($audio.find("div.play_btn input[type=hidden]").val());
     var song = $audio.find(".title_wrap").text();
     return {song: song, url: url};
 }
 
-function every(milliseconds, callback) {
-    callback();
-    var again = function () {
-        every(milliseconds, callback);
-    };
-    setTimeout(again, milliseconds);
-}
 
 function currentlyPlayedUiItem() {
-    var $audio = $(".audio:has(.airplay-button.pause)");
+    var $audio = $(".audio:has(.airplay-button.being-played)");
     if ($audio.length) {
         return scrap($audio);
     } else {
@@ -39,47 +39,20 @@ function currentlyPlayedUiItem() {
     }
 }
 
-function finished(item) {
-    var serverStatus = status();
-    return (serverStatus) &&
-        (serverStatus.position > serverStatus.length - 5) &&
-        (serverStatus.url == item.url);
+function markAsBeingPlayed($audio) {
+    $audio.find('.airplay-button').removeClass("not-being-played").addClass("being-played");
 }
 
-function playNextIfCurrentSongFinished() {
-    var currentItem = currentlyPlayedUiItem();
-    if (currentItem && finished(currentItem)) {
-        playNextAfter(currentItem);
-    }
-}
-
-// TODO: refactor
-function playNextAfter(currentItem) {
-    var playNext = false;
-    $audios_list.find(".audio.airplay-hooked").each(function () {
-        var item = scrap($(this));
-        if (playNext) {
-            $(this).find(".airplay-button").click();
-        }
-        if (item.url == currentItem.url) {
-            playNext = true;
-        } else {
-            playNext = false;
-        }
-    });
-}
-
-function clearPlayIcons() {
-    $(".airplay-button.pause").removeClass("pause").addClass("triangle");
+function onAudioClicked($audio) {
+    play(scrap($audio));
+    clearPlayIcons();
+    markAsBeingPlayed($audio);
 }
 
 function setHandlers($audio) {
-    $audio.find(".airplay-button.triangle").click(function (event) {
+    $audio.find(".airplay-button.not-being-played").click(function (event) {
         event.stopPropagation();
-        var $audio = $(this).parent();
-        play(scrap($audio));
-        clearPlayIcons();
-        $(this).removeClass("triangle").addClass("pause");
+        onAudioClicked($(this).parent());
     });
 
     $audio.find(".airplay-button.pause").click(function (event) {
@@ -89,13 +62,90 @@ function setHandlers($audio) {
     });
 }
 
+function scrollToAudio($audio) {
+    $('html,body').animate({
+        scrollTop: $audio.offset().top
+    });
+}
+
+function clearPlayIcons() {
+    $(".airplay-button.being-played").removeClass("being-played").addClass("not-being-played");
+}
+
+
+// MODEL FUNCTIONS, in terms of MVC
+
+function justFinished(item, serverStatus) {
+    return (serverStatus) &&
+        (serverStatus.position > serverStatus.length - 3) &&
+        (serverStatus.status == "pause") &&
+        (serverStatus.url == item.url);
+}
+
+function syncStateWithServer() {
+    // after the server finished playing, the item is still displayed as 'playing' in the UI
+    var currentItem = currentlyPlayedUiItem();
+    var serverStatus = status();
+    if (currentItem && justFinished(currentItem, serverStatus)) {
+        playNextAfter(currentItem);
+    }
+    highlightAudioCurrentlyPlayedOnServer(serverStatus);
+}
+
+// TODO: refactor
+function playNextAfter(currentItem) {
+    var playNext = false;
+    $audios_list.find(".audio.airplay-hooked").each(function () {
+        if (playNext) {
+            onAudioClicked($(this));
+            scrollToAudio($(this)); // just to initiate VK's infinite scroll
+        }
+        var item = scrap($(this));
+        if (item.url == currentItem.url) {
+            playNext = true;
+        } else {
+            playNext = false;
+        }
+    });
+}
+
+function audioChosenOnServer(serverStatus) {
+    var result = null;
+    $audios_list.find(".audio.airplay-hooked").each(function () {
+        var $audio = $(this);
+        if (serverStatus.url == scrap($audio).url) {
+            result = $audio;
+        }
+    });
+    return result;
+}
+
+function highlightAudioCurrentlyPlayedOnServer(serverStatus) {
+    if (serverStatus.status == "play") {
+        var $audio = audioChosenOnServer(serverStatus);
+        if ($audio) {
+            markAsBeingPlayed($audio);
+        }
+    }
+}
+
+// IN MVC, THIS WOULD BE CONTROLLER FUNCTIONS
+
+function every(milliseconds, callback) {
+    callback();
+    var again = function () {
+        every(milliseconds, callback);
+    };
+    setTimeout(again, milliseconds);
+}
+
+
 $(function () {
     $audios_list = $("#audios_list");
 
-    every(1000, function () {
-        playNextIfCurrentSongFinished();
-        rescanAndHookDom();
-    });
+    // fires the callbacks immediately for the first time
+    every(3000, rescanAndHookDom);
+    every(3000, syncStateWithServer);
 });
 
 
